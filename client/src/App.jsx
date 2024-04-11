313746a45da248f533314bb601186ecd03aa136e
Donald Ballowe <dballowe7912@gmail.com>
2024-04-11 02:39:29 -0500

    Added Blogs page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
 import Error from "./pages/Error";
 
 const router = createBrowserRouter([
 	{ path: "/", Component: Home, errorElement: Error },
+	{ path: "/blogs", Component: Blogs },
 ]);
 
 function App() {
