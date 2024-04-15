de3ddeebfaf44dab5e7310ec7972df656a313a75
Donald Ballowe <dballowe7912@gmail.com>
2024-04-15 17:28:02 -0500

    Added getBlogById and getBlogInCategory controllers and routes to backend

--- server/controllers/blog.controllers.js
+++ server/controllers/blog.controllers.js
@@ -5,6 +5,19 @@ async function getAllblogs(req, res) {
 	res.send(blogs);
 }
 
+async function getBlogById(req, res) {
+	const blog = await Blog.findById(req.params.id);
+	res.send(blog);
+}
+
+async function getBlogsInCategory(req, res) {
+	const category = req.params.category;
+	const blog = await Blog.find({ category });
+	res.send(blog);
+}
+
 module.exports = {
 	getAllblogs,
+	getBlogById,
+	getBlogsInCategory,
 };
--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const catchAsync = require("../utils/catchAsync");
 
-const { getAllblogs } = require("../controllers/blog.controllers");
+const {
+	getAllblogs,
+	getBlogById,
+	getBlogsInCategory,
+} = require("../controllers/blog.controllers");
 
 const router = express.Router();
 
 router.get("/blogs", catchAsync(getAllblogs));
+router.get("/blogs/:id", catchAsync(getBlogById));
+router.get("/blogs/category/:category", catchAsync(getBlogsInCategory));
 
 module.exports = router;
