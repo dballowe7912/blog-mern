a0f63ef13cc78495e5dd15e2f7f8eb4dc0986fb3
Donald Ballowe <dballowe7912@gmail.com>
2024-04-15 12:45:46 -0500

    Added user model and seeds to server

---
+++ seeds/index.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const blogs = require("./blogs");
+const users = require("./users");
+const Blog = require("../server/models/blog.model");
+const User = require("../server/models/user.model");
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/mern-blog";
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
+const seedDB = async () => {
+	await Blog.deleteMany({});
+	await User.deleteMany({});
+	for (let i = 0; i < blogs.length; i++) {
+		const b = new Blog({
+			title: blogs[i].title,
+			body: blogs[i].body,
+			author: blogs[i].author,
+			date: blogs[i].date,
+			image: blogs[i].image,
+			category: blogs[i].category,
+		});
+		await b.save();
+	}
+	for (let i = 0; i < users.length; i++) {
+		const u = new User({
+			username: users[i].username,
+			email: users[i].email,
+			password: users[i].password,
+			isAdmin: users[i].isAdmin,
+		});
+		await u.save();
+	}
+};
+
+seedDB().then(() => {
+	mongoose.connection.close();
+});
---
+++ seeds/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+	{
+		username: "admin_user",
+		email: "admin@gmail.com",
+		password: "123456",
+		isAdmin: true,
+	},
+	{
+		username: "john_doe",
+		email: "john@gmail.com",
+		password: "123456",
+		isAdmin: false,
+	},
+	{
+		username: "jane_doe",
+		email: "jane@gmail.com",
+		password: "123456",
+		isAdmin: false,
+	},
+];
