424bf83404934e55a4df2b7f94680df8912490e4
Donald Ballowe <dballowe7912@gmail.com>
2024-04-19 22:04:54 -0500

    Added Single page and route to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Error from "./pages/Error";
+import Single from "./pages/Single";
 
 const router = createBrowserRouter([
 	{ path: "/", Component: Home, errorElement: Error },
 	{ path: "/blogs", Component: Blogs },
+	{ path: "/single", Component: Single },
 ]);
 
 function App() {
