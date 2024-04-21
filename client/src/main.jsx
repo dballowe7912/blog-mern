dffb4e3f8fc45f8092e2ff7a5ba3654f5b546ce4
Donald Ballowe <dballowe7912@gmail.com>
2024-04-21 06:01:18 -0500

    Wrapped App in BlogProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import BlogProvider from "./context/BlogState/BlogProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<BlogProvider>
+			<App />
+		</BlogProvider>
 	</React.StrictMode>
 );
