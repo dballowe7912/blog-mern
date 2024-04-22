edc9c05c8ce149150086aaac132ffaf3f260db5f
Donald Ballowe <dballowe7912@gmail.com>
2024-04-22 01:52:45 -0500

    Added About page with AOS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Error from "./pages/Error";
 import Single from "./pages/Single";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
 	{ path: "/", Component: Home, errorElement: Error },
 	{ path: "/blogs", Component: Blogs },
 	{ path: "/single", Component: Single },
+	{ path: "/about", Component: About },
 ]);
 
 function App() {
