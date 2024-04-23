1d53f1ece67405c773e7f6da87190123443b02c5
Donald Ballowe <dballowe7912@gmail.com>
2024-04-23 03:47:10 -0500

    Moved fetchBlogs logic to context

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { useContext } from "react";
+import { BlogContext } from "../context/BlogState/BlogContext";
+
 import MobileNav from "../components/MobileNav";
 import Navbar from "../components/Navbar";
-
-// import Preloader from "../components/Preloader";
 import Hero from "../components/Hero";
 import BlogEntry from "../components/BlogEntry";
 import Pagination from "../components/Pagination";
@@ -14,22 +11,10 @@ import SidebarPopularPosts from "../components/sidebars/SidebarPopularPosts";
 import SidebarCategories from "../components/sidebars/SidebarCategories";
 import SidebarTags from "../components/sidebars/SidebarTags";
 import Footer from "../components/Footer";
+// import Preloader from "../components/Preloader";
 
 function Blog() {
-	const [blogs, setBlogs] = useState([]);
-	const fetchBlogs = () => {
-		axios.get("http://127.0.0.1:8080/blogs").then((res) => {
-			setBlogs(res.data);
-		});
-	};
-
-	useEffect(() => {
-		fetchBlogs();
-		AOS.init({
-			duration: 1200,
-			easing: "ease-in",
-		});
-	}, [blogs]);
+	const blogs = useContext(BlogContext);
 
 	return (
 		<>
