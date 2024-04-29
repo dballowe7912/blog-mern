362aede09573a5c7ebde6b4f2b1c66da62b2b8aa
Donald Ballowe <dballowe7912@gmail.com>
2024-04-28 20:16:44 -0500

    Removed body-parser, using express.json instead

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const morgan = require("morgan");
 
@@ -19,8 +18,7 @@ if (NODE_ENV === "development") {
 }
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api/blogs", require("./routes/blog.routes"));
 
