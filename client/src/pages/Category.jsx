ba1bb80b63c079164a96a4ed4ed62cfdccbbe995
Donald Ballowe <dballowe7912@gmail.com>
2024-04-28 20:17:23 -0500

    Splitting Category.jsx

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -1,5 +1,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import SidebarCategories from "../components/sidebars/SidebarCategories";
+import SidebarTags from "../components/sidebars/SidebarTags";
+import SidebarPopularPosts from "../components/sidebars/SidebarPopularPosts";
+import SidebarSearch from "../components/sidebars/SidebarSearch";
+import Pagination from "../components/Pagination";
 
 const Category = () => {
 	return (
@@ -188,173 +193,14 @@ const Category = () => {
 									</p>
 								</div>
 							</div>
-
-							<div className="row text-start pt-5 border-top">
-								<div className="col-md-12">
-									<div className="custom-pagination">
-										<span>1</span>
-										<a href="#">2</a>
-										<a href="#">3</a>
-										<a href="#">4</a>
-										<span>...</span>
-										<a href="#">15</a>
-									</div>
-								</div>
-							</div>
+							<Pagination />
 						</div>
 
 						<div className="col-lg-4 sidebar">
-							<div className="sidebar-box search-form-wrap mb-4">
-								<form
-									action="#"
-									className="sidebar-search-form"
-								>
-									<span className="bi-search"></span>
-									<input
-										type="text"
-										className="form-control"
-										id="s"
-										placeholder="Type a keyword and hit enter"
-									/>
-								</form>
-							</div>
-							{/* <!-- END sidebar-box --> */}
-							<div className="sidebar-box">
-								<h3 className="heading">Popular Posts</h3>
-								<div className="post-entry-sidebar">
-									<ul>
-										<li>
-											<a href="">
-												<img
-													src="images/img_1_sq.jpg"
-													alt="Image placeholder"
-													className="me-4 rounded"
-												/>
-												<div className="text">
-													<h4>
-														There is a Cool New Way for Men to Wear Socks and
-														Sandals
-													</h4>
-													<div className="post-meta">
-														<span className="mr-2">March 15, 2018 </span>
-													</div>
-												</div>
-											</a>
-										</li>
-										<li>
-											<a href="">
-												<img
-													src="images/img_2_sq.jpg"
-													alt="Image placeholder"
-													className="me-4 rounded"
-												/>
-												<div className="text">
-													<h4>
-														There’s a Cool New Way for Men to Wear Socks and
-														Sandals
-													</h4>
-													<div className="post-meta">
-														<span className="mr-2">March 15, 2018 </span>
-													</div>
-												</div>
-											</a>
-										</li>
-										<li>
-											<a href="">
-												<img
-													src="images/img_3_sq.jpg"
-													alt="Image placeholder"
-													className="me-4 rounded"
-												/>
-												<div className="text">
-													<h4>
-														There’s a Cool New Way for Men to Wear Socks and
-														Sandals
-													</h4>
-													<div className="post-meta">
-														<span className="mr-2">March 15, 2018 </span>
-													</div>
-												</div>
-											</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-							{/* <!-- END sidebar-box --> */}
-
-							<div className="sidebar-box">
-								<h3 className="heading">Categories</h3>
-								<ul className="categories">
-									<li>
-										<a href="#">
-											Food <span>(12)</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Travel <span>(22)</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Lifestyle <span>(37)</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Business <span>(42)</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Adventure <span>(14)</span>
-										</a>
-									</li>
-								</ul>
-							</div>
-							{/* <!-- END sidebar-box --> */}
-
-							<div className="sidebar-box">
-								<h3 className="heading">Tags</h3>
-								<ul className="tags">
-									<li>
-										<a href="#">Travel</a>
-									</li>
-									<li>
-										<a href="#">Adventure</a>
-									</li>
-									<li>
-										<a href="#">Food</a>
-									</li>
-									<li>
-										<a href="#">Lifestyle</a>
-									</li>
-									<li>
-										<a href="#">Business</a>
-									</li>
-									<li>
-										<a href="#">Freelancing</a>
-									</li>
-									<li>
-										<a href="#">Travel</a>
-									</li>
-									<li>
-										<a href="#">Adventure</a>
-									</li>
-									<li>
-										<a href="#">Food</a>
-									</li>
-									<li>
-										<a href="#">Lifestyle</a>
-									</li>
-									<li>
-										<a href="#">Business</a>
-									</li>
-									<li>
-										<a href="#">Freelancing</a>
-									</li>
-								</ul>
-							</div>
+							<SidebarSearch />
+							<SidebarPopularPosts />
+							<SidebarCategories />
+							<SidebarTags />
 						</div>
 					</div>
 				</div>
